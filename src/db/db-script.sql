ae2a4caadb7042142873a3f2d2b0e9df1392f772
Plastaaa <80416635+Plastaaa@users.noreply.github.com>
2023-07-07 14:52:44 +0200

    Update db-script.sql

--- src/db/db-script.sql
+++ src/db/db-script.sql
@@ -1,3 +1,3 @@
 CREATE TABLE `dbtpbackjs`.`auteur` (`id` INT NOT NULL AUTO_INCREMENT , `nom` VARCHAR(64) NOT NULL , `prenom` VARCHAR(64) NOT NULL , `genreLitt` VARCHAR(64) NOT NULL , `age` INT(3) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
 
-INSERT INTO `auteur` (`id`, `nom`, `prenom`, `genreLitt`, `age`) VALUES ('1', 'Test', 'Michel', 'Policier', '99'), ('2', 'Robert', 'Miche', 'Jeunesse', '67'), ('3', 'Alfred', 'Baidou', 'Roman', '79'), ('4', 'Alicia', 'Uizad', 'Psychologie', '32'), ('5', 'Guy', 'Frand', 'Policier', '99');
+INSERT INTO `auteur` (`id`, `nom`, `prenom`, `genreLitt`, `age`) VALUES ('1', 'Dupont', 'Michel', 'Policier', '99'), ('2', 'Miche', 'Robert', 'Jeunesse', '67'), ('3', 'Baidou', 'Alfred', 'Roman', '79'), ('4', 'Uizad', 'Alicia', 'Psychologie', '32'), ('5', 'Frand', 'Guy', 'Policier', '99');
